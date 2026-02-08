16b15cbfb840668e945332f3739f01764f021a6a
MGlibya03 <mghadim683@gmail.com>
2026-02-08 21:49:39 +0200

    Update admin_status_helpers.py
    Signed-off-by: MGlibya03 <mghadim683@gmail.com>

--- tg_bot/modules/helper_funcs/admin_status_helpers.py
+++ tg_bot/modules/helper_funcs/admin_status_helpers.py
@@ -1,7 +1,7 @@
 # admin status module by Luke (@itsLuuke - t.me/itsLuuke)
 # written for OdinRobot
 # copyright 2022
-# this module contains various helper functions/classes to help with the admin status module
+# معرب لزورو بوت
 
 from enum import Enum
 from cachetools import TTLCache
@@ -11,10 +11,10 @@ from telegram import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, 
 from tg_bot import OWNER_ID, SYS_ADMIN, DEV_USERS, MOD_USERS, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS
 
 # stores admin in memory for 10 min.
-ADMINS_CACHE = TTLCache(maxsize = 512, ttl = 60 * 30)
+ADMINS_CACHE = TTLCache(maxsize=512, ttl=60 * 30)
 
 # stores bot admin status in memory for 10 min.
-BOT_ADMIN_CACHE = TTLCache(maxsize = 512, ttl = 60 * 30)
+BOT_ADMIN_CACHE = TTLCache(maxsize=512, ttl=60 * 30)
 
 DEV_USERS = DEV_USERS
 
@@ -28,70 +28,60 @@ MOD_USERS = MOD_USERS + SUDO_USERS
 
 
 class AdminPerms(Enum):
-	CAN_RESTRICT_MEMBERS = 'can_restrict_members'
-	CAN_PROMOTE_MEMBERS = 'can_promote_members'
-	CAN_INVITE_USERS = 'can_invite_users'
-	CAN_DELETE_MESSAGES = 'can_delete_messages'
-	CAN_CHANGE_INFO = 'can_change_info'
-	CAN_PIN_MESSAGES = 'can_pin_messages'
-	IS_ANONYMOUS = 'is_anonymous'
+    CAN_RESTRICT_MEMBERS = 'can_restrict_members'
+    CAN_PROMOTE_MEMBERS = 'can_promote_members'
+    CAN_INVITE_USERS = 'can_invite_users'
+    CAN_DELETE_MESSAGES = 'can_delete_messages'
+    CAN_CHANGE_INFO = 'can_change_info'
+    CAN_PIN_MESSAGES = 'can_pin_messages'
+    IS_ANONYMOUS = 'is_anonymous'
 
 
 class ChatStatus(Enum):
-	CREATOR = "creator"
-	ADMIN = "administrator"
-
-
-# class SuperUsers(Enum):
-# 	Owner = [OWNER_ID]
-# 	SysAdmin = [OWNER_ID, SYS_ADMIN]
-# 	Devs = DEV_USERS
-# 	Sudos = SUDO_USERS
-# 	Supports = SUPPORT_USERS
-# 	Whitelist = WHITELIST_USERS
-# 	Mods = MOD_USERS
+    CREATOR = "creator"
+    ADMIN = "administrator"
 
 
 def anon_reply_markup(cb_id: str) -> InlineKeyboardMarkup:
-	return InlineKeyboardMarkup(
-			[
-				[
-					InlineKeyboardButton(
-							text = 'Prove identity',
-							callback_data = cb_id
-					)
-				]
-			]
-	)
+    return InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text='اثبت هويتك 👤',
+                    callback_data=cb_id
+                )
+            ]
+        ]
+    )
 
 
-anon_reply_text = "Seems like you're anonymous, click the button below to prove your identity"
+anon_reply_text = "🔒 يبدو انك مجهول الهوية!\nاضغط الزر تحت باش تثبت هويتك:"
 
 
 def edit_anon_msg(msg: Message, text: str):
-	"""
-	edit anon check message and remove the button
-	"""
-	msg.edit_text(text, parse_mode = ParseMode.MARKDOWN, reply_markup = None)
+    """
+    edit anon check message and remove the button
+    """
+    msg.edit_text(text, parse_mode=ParseMode.MARKDOWN, reply_markup=None)
 
 
 def user_is_not_admin_errmsg(msg: Message, permission: AdminPerms = None, cb: CallbackQuery = None):
-	if permission.value:
-		errmsg = f"You lack the following permission for this command:\n`{permission.value}`!"
-	else:
-		errmsg = f"You lack the necessary permission needed for this command!"
-	if cb:
-		return cb.answer(errmsg, show_alert = True)
-	return msg.reply_text(errmsg, parse_mode = ParseMode.MARKDOWN)
+    if permission and permission.value:
+        errmsg = f"⛔ ما عندك الصلاحية المطلوبة:\n`{permission.value}`!"
+    else:
+        errmsg = f"⛔ ما عندك الصلاحية اللازمة لهذا الامر!"
+    if cb:
+        return cb.answer(errmsg, show_alert=True)
+    return msg.reply_text(errmsg, parse_mode=ParseMode.MARKDOWN)
 
 
 def button_expired_error(u: Update):
-	errmsg = f"This button has expired!"
-	if u.callback_query:
-		u.callback_query.answer(errmsg, show_alert = True)
-		u.effective_message.delete()
-		return
-	return u.effective_message.edit_text(errmsg, parse_mode = ParseMode.MARKDOWN)
+    errmsg = f"❌ هذا الزر منتهي الصلاحية!"
+    if u.callback_query:
+        u.callback_query.answer(errmsg, show_alert=True)
+        u.effective_message.delete()
+        return
+    return u.effective_message.edit_text(errmsg, parse_mode=ParseMode.MARKDOWN)
 
 
 anon_callbacks = {}
